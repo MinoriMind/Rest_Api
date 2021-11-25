9abc905cff32abea3ed6f83be7e2442d68fe09d2
MinoriMind <similianka@gmail.com>
2021-11-26 01:07:58 +1000

    Create todo

---
+++ src/Controller/TodoController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Todo;
+use App\Repository\TodoRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+
+#[Route('/todo', name: 'todo')]
+class TodoController extends AbstractController
+{
+    #[Route('/', name: 'create', methods:["POST"])]
+    public function create(Request $request, UserRepository $user_rep): Response
+    {
+        $content_type = $request->getContentType();
+        if ($content_type != 'json') {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Only application/json content type is allowed',
+            ]);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        if (json_last_error() != JSON_ERROR_NONE) {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Error during parsing json',
+            ]);
+        }
+
+        if(!isset($data['login']))
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Login not found',
+            ]);
+        }
+
+        if(!isset($data['password']))
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Password not found',
+            ]);
+        }
+
+        if(!isset($data['name']))
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Name not found',
+            ]);
+        }
+
+        if(!isset($data['text']))
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Text not found',
+            ]);
+        }
+
+        $login = $data['login'];
+        $password = $data['password'];
+
+        $user = $user_rep->findOneBy([
+            'login' => $login,
+            'password' => hash('sha256', $password)
+        ]);
+
+        if(!$user)
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => 'User not found',
+            ]);
+        }
+
+        $todo = new Todo();
+        $todo->setName($data["name"]);
+        $todo->setText($data["text"]);
+        $todo->setUser($user);
+
+        $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
+        try 
+        {
+            $em->persist($todo);
+            $em->flush();
+        } 
+        catch (UniqueConstraintViolationException $exception)
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => 'Todo with same name exists',
+            ]);
+        }
+
+        return $this->json([
+            'status' => 200,
+            'message' => 'Todo successfully added',
+        ]);
+
+    }
+}
---
+++ src/Entity/Todo.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="todos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
